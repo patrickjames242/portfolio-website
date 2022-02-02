8a4623f8638d413b2b194bf66936084d8f9e1607
Patrick Hanna <contact@patrickhanna.dev>
2022-02-02 12:57:02 -0500

    updated technologies on about me section

--- src/App/children/AboutMeSection/technologies.ts
+++ src/App/children/AboutMeSection/technologies.ts
@@ -1,7 +1,9 @@
 import angular from "./technologyIcons/angular.svg";
 import css from "./technologyIcons/css.svg";
 import django from "./technologyIcons/django.svg";
+import firebase from "./technologyIcons/firebase.svg";
 import git from "./technologyIcons/git.svg";
+import heroku from "./technologyIcons/heroku.svg";
 import html from "./technologyIcons/html.svg";
 import javascript from "./technologyIcons/javascript.svg";
 import nodeJs from "./technologyIcons/nodejs.svg";
@@ -9,6 +11,7 @@ import postgres from "./technologyIcons/postgresql.svg";
 import python from "./technologyIcons/python.svg";
 import reactNative from "./technologyIcons/react-native.svg";
 import react from "./technologyIcons/react.svg";
+import redux from "./technologyIcons/redux.svg";
 import sass from "./technologyIcons/sass.svg";
 import swift from "./technologyIcons/swift.svg";
 import typescript from "./technologyIcons/typescript.svg";
@@ -25,11 +28,14 @@ const technologies: { name: string; iconSrc: string }[] = [
 	{ name: "TypeScript", iconSrc: typescript },
 	{ name: "Sass", iconSrc: sass },
 	{ name: "React", iconSrc: react },
+	{ name: "Redux", iconSrc: redux },
 	{ name: "Angular", iconSrc: angular },
 	{ name: "NodeJS", iconSrc: nodeJs },
 	{ name: "Python", iconSrc: python },
 	{ name: "Django", iconSrc: django },
 	{ name: "PostgreSQL", iconSrc: postgres },
+	{ name: "Firebase", iconSrc: firebase },
+	{ name: "Heroku", iconSrc: heroku },
 ];
 
 export default technologies;
