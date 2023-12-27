9d5f977e838f7c3e71e85ba47d4fa47d7d8fc8d8
Patrick Hanna <contact@patrickhanna.dev>
2023-12-27 10:34:25 -0500

    updated order of projects

--- src/App/MainScreen/children/ProjectsSection/projectsData.tsx
+++ src/App/MainScreen/children/ProjectsSection/projectsData.tsx
@@ -152,6 +152,94 @@ export const allProjects: Project[] = [
     ],
   }),
   project({
+    imageUrls: [betterLivingAppImage],
+    title: 'Better Living App',
+    subtitle: 'Cross Platform App',
+    technologies: [
+      tech.reactNative,
+      tech.typescript,
+      tech.python,
+      tech.django,
+      tech.postgres,
+    ],
+    description: (
+      <>
+        This app allows users to place food orders at Better Living Health
+        Center and browse a collection of health tip videos prepared by the
+        proprietor. The app also features a{' '}
+        <a
+          className="underline-on-hover"
+          href="https://en.wikipedia.org/wiki/Content_management_system"
+          target="_blank"
+          rel="noreferrer"
+        >
+          CMS
+        </a>{' '}
+        that allows staff to update information in the app in realtime.
+      </>
+    ),
+    githubLink: 'https://github.com/patrickjames242/better-living-app',
+    websiteLink: 'https://app.betterlivingnassau.com/',
+    playStoreLink:
+      'https://play.google.com/store/apps/details?id=com.patrickhanna.betterliving',
+    appStoreLink:
+      'https://apps.apple.com/lr/app/better-living-health-center/id1541047557',
+    documentViewerItems: [
+      { data: documentViewerItemImage(betterLivingAppImage) },
+    ],
+  }),
+  project({
+    imageUrls: [betterLivingImage],
+    title: 'Better Living Website',
+    subtitle: 'Website Landing Page',
+    technologies: [tech.react, tech.typescript, tech.scss],
+    description: (
+      <>
+        This is the official website for Better Living Health Center & Deli. It
+        was built for the purpose of promoting and marketing the health center
+        as well as providing useful information about it.
+      </>
+    ),
+    githubLink: 'https://github.com/patrickjames242/better-living-website',
+    websiteLink: 'https://betterlivingnassau.com/',
+    documentViewerItems: [
+      { data: documentViewerWebsiteDemo('https://betterlivingnassau.com') },
+    ],
+  }),
+  project({
+    imageUrls: [screwsWorldImage],
+    title: 'Screws World Website',
+    subtitle: 'Website Landing Page',
+    technologies: [
+      tech.typescript,
+      tech.react,
+      tech.scss,
+      tech.nodeJS,
+      tech.express,
+    ],
+    description: (
+      <>
+        {
+          "This serves as the official website of Screws and Fastener's world. It's purpose is to market the business and provide customers with a catalog of available products. It also features a "
+        }
+        <a
+          className="underline-on-hover"
+          href="https://en.wikipedia.org/wiki/Content_management_system"
+          target="_blank"
+          rel="noreferrer"
+        >
+          CMS
+        </a>{' '}
+        that allows the proprietor to update the product catalogue.
+      </>
+    ),
+    githubLink: 'https://github.com/patrickjames242/screws-world-website',
+    websiteLink: 'https://screwsworldbahamas.com/',
+    documentViewerItems: [
+      { data: documentViewerWebsiteDemo('https://screwsworldbahamas.com') },
+    ],
+  }),
+  project({
     imageUrls: [
       {
         url: teleconnectImage,
@@ -200,94 +288,7 @@ export const allProjects: Project[] = [
       },
     ],
   }),
-  project({
-    imageUrls: [betterLivingImage],
-    title: 'Better Living Website',
-    subtitle: 'Website Landing Page',
-    technologies: [tech.react, tech.typescript, tech.scss],
-    description: (
-      <>
-        This is the official website for Better Living Health Center & Deli. It
-        was built for the purpose of promoting and marketing the health center
-        as well as providing useful information about it.
-      </>
-    ),
-    githubLink: 'https://github.com/patrickjames242/better-living-website',
-    websiteLink: 'https://betterlivingnassau.com/',
-    documentViewerItems: [
-      { data: documentViewerWebsiteDemo('https://betterlivingnassau.com') },
-    ],
-  }),
-  project({
-    imageUrls: [betterLivingAppImage],
-    title: 'Better Living App',
-    subtitle: 'Cross Platform App',
-    technologies: [
-      tech.reactNative,
-      tech.typescript,
-      tech.python,
-      tech.django,
-      tech.postgres,
-    ],
-    description: (
-      <>
-        This app allows users to place food orders at Better Living Health
-        Center and browse a collection of health tip videos prepared by the
-        proprietor. The app also features a{' '}
-        <a
-          className="underline-on-hover"
-          href="https://en.wikipedia.org/wiki/Content_management_system"
-          target="_blank"
-          rel="noreferrer"
-        >
-          CMS
-        </a>{' '}
-        that allows staff to update information in the app in realtime.
-      </>
-    ),
-    githubLink: 'https://github.com/patrickjames242/better-living-app',
-    websiteLink: 'https://app.betterlivingnassau.com/',
-    playStoreLink:
-      'https://play.google.com/store/apps/details?id=com.patrickhanna.betterliving',
-    appStoreLink:
-      'https://apps.apple.com/lr/app/better-living-health-center/id1541047557',
-    documentViewerItems: [
-      { data: documentViewerItemImage(betterLivingAppImage) },
-    ],
-  }),
-  project({
-    imageUrls: [screwsWorldImage],
-    title: 'Screws World Website',
-    subtitle: 'Website Landing Page',
-    technologies: [
-      tech.typescript,
-      tech.react,
-      tech.scss,
-      tech.nodeJS,
-      tech.express,
-    ],
-    description: (
-      <>
-        {
-          "This serves as the official website of Screws and Fastener's world. It's purpose is to market the business and provide customers with a catalog of available products. It also features a "
-        }
-        <a
-          className="underline-on-hover"
-          href="https://en.wikipedia.org/wiki/Content_management_system"
-          target="_blank"
-          rel="noreferrer"
-        >
-          CMS
-        </a>{' '}
-        that allows the proprietor to update the product catalogue.
-      </>
-    ),
-    githubLink: 'https://github.com/patrickjames242/screws-world-website',
-    websiteLink: 'https://screwsworldbahamas.com/',
-    documentViewerItems: [
-      { data: documentViewerWebsiteDemo('https://screwsworldbahamas.com') },
-    ],
-  }),
+
   project({
     imageUrls: [chatCamImage],
     title: 'ChatCam',
